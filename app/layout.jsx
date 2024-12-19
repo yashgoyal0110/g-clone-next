92a1c5139c66e0a0f609a33e40acb77dc81598fc
yash goyal <yg364550@gmail.com>
2024-12-20 04:18:51 +0530

    added google favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,6 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="images/favicon.ico"/>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AppProvider>
         {children}
